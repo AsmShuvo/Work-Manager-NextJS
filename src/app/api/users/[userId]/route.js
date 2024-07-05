ef7cb35b9c3a29c8c7f65594176bcabb14b81467
AsmShuvo <hoffmanhulk99@gmail.com>
2024-07-06 02:42:57 +0600

    delete: single user

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,8 +1,28 @@
+import { User } from "@/models/user";
+import { CommandSucceededEvent } from "mongodb";
 import { NextResponse } from "next/server";
 
-export const DELETE = (request, { params }) => {
+export const GET = async (req, { params }) => {
   const { userId } = params;
-  return NextResponse.json({
-    message: `id is ${userId}`,
-  });
+  const user = await User.findById(userId);
+  return NextResponse.json(user);
+};
+
+export const DELETE = async (request, { params }) => {
+  const { userId } = params;
+  try {
+    await User.deleteOne({
+      _id: userId,
+    });
+    return NextResponse.json({
+      message: "User deleted",
+      success: true,
+    });
+  } catch (err) {
+    console.log(`ERROR while deleting id : ${userId}`);
+    return NextResponse.json({
+      message: "User deleted",
+      success: true,
+    });
+  }
 };
