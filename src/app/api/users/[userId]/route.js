286af407096def976c2936fbb9985a2a5ee22f99
AsmShuvo <hoffmanhulk99@gmail.com>
2024-07-06 03:18:12 +0600

    up: user updated

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -26,3 +26,24 @@ export const DELETE = async (request, { params }) => {
     });
   }
 };
+
+export const PUT = async (request, { params }) => {
+  const { userId } = params;
+  const { name, about, profileURL, email } = await request.json();
+  try {
+    const user = await User.findById(userId);
+    user.name = name;
+    user.about = about;
+    user.email = email;
+    user.profileURL = profileURL;
+    const updatedUser = await user.save();
+    return NextResponse.json({
+      message: "User updated !",
+      success: false,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      message: "user not updated",
+    });
+  }
+};
